f1d6b201645540fa24c156a6058a3b877839db5f
Elsa Serrano <elsa2304014@hybridge.education>
2023-07-19 20:54:27 -0600

    agregar archivo tv y main

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import os
+from tv import TV
+
+television1 = TV("Sony", 56)
+
+while True:
+    print("Control remoto")
+    print("1. Encender")
+    print("2. Subir Volumen")
+    print("3. Bajar Volumen")
+    print("4. Cambiar Canal")
+    boton = input("Presiona un botón: ")
+    if boton == "1":
+        television1.power()
+    elif boton == "2":
+        television1.cambiar_volumen(1)
+    elif boton == "3":
+        television1.cambiar_volumen(-1)
+    
+    time.sleep(1)
+    print(television1.estatus_actual())
+    input("Presiona enter para continuar...")
+    os.system("clear")
